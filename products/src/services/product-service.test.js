b5a61779f820dd41b8112d64e16d84241cb8bad0
jay <codergogoi@gmail.com>
2022-09-18 14:30:46 +0200

    Added test Cases Configurations

---
+++ products/src/services/product-service.test.js
@@ -0,0 +1,10 @@
+// which service it is
+describe("ProductService", () => {
+  // Which function
+  describe("CreateProduct", () => {
+    // Which Scenario we are testing
+    test("validate user inputs", () => {});
+
+    test("Validate response", async () => {});
+  });
+});
