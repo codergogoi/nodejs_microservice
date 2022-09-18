0b3817506bdf5e17d0f38c33e83ca2d918a0da31
jay <codergogoi@gmail.com>
2022-09-18 14:31:14 +0200

    Added test Cases Configurations

---
+++ shopping/src/services/shopping-service.test.js
@@ -0,0 +1,10 @@
+// which service it is
+describe("ShoppingService", () => {
+  // Which function
+  describe("PlaceOrder", () => {
+    // Which Scenario we are testing
+    test("validate user inputs", () => {});
+
+    test("Validate response", async () => {});
+  });
+});
