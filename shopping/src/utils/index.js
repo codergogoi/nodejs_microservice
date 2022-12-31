1591e7e29c545a0b36cb2aeb3d5122491017d75e
jay <codergogoi@gmail.com>
2022-12-31 15:06:28 +0100

    Updated signature validation function

--- shopping/src/utils/index.js
+++ shopping/src/utils/index.js
@@ -10,12 +10,13 @@ const {
 } = require("../config");
 
 //Utility functions
-(module.exports.GenerateSalt = async () => {
+module.exports.GenerateSalt = async () => {
   return await bcrypt.genSalt();
-}),
-  (module.exports.GeneratePassword = async (password, salt) => {
-    return await bcrypt.hash(password, salt);
-  });
+};
+
+module.exports.GeneratePassword = async (password, salt) => {
+  return await bcrypt.hash(password, salt);
+};
 
 module.exports.ValidatePassword = async (
   enteredPassword,
@@ -25,20 +26,27 @@ module.exports.ValidatePassword = async (
   return (await this.GeneratePassword(enteredPassword, salt)) === savedPassword;
 };
 
-(module.exports.GenerateSignature = async (payload) => {
-  return await jwt.sign(payload, APP_SECRET, { expiresIn: "90d" });
-}),
-  (module.exports.ValidateSignature = async (req) => {
+module.exports.GenerateSignature = async (payload) => {
+  try {
+    return await jwt.sign(payload, APP_SECRET, { expiresIn: "30d" });
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+module.exports.ValidateSignature = async (req) => {
+  try {
     const signature = req.get("Authorization");
-
-    if (signature) {
-      const payload = await jwt.verify(signature.split(" ")[1], APP_SECRET);
-      req.user = payload;
-      return true;
-    }
-
+    console.log(signature);
+    const payload = await jwt.verify(signature.split(" ")[1], APP_SECRET);
+    req.user = payload;
+    return true;
+  } catch (error) {
+    console.log(error);
     return false;
-  });
+  }
+};
 
 module.exports.FormateData = (data) => {
   if (data) {
